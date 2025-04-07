ac85c6d56de9dbd2cb344d55342fbd7609a652fd
KumarSamarth-ofc <kumarsamarth.adept@gmail.com>
2025-04-07 19:12:52 +0530

    write and read to firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Write from "./components/Write";
+import Read from "./components/Read";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Write />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/read" element={<Read />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
